38619fe1478d58e5197c455bdc30aaf4d9f33edf
Dave Ryerson <dave.ryerson@alexion.com>
2026-05-11 13:13:38 -0700

    Pushed from Domino: https://domino.astrazeneca.net/workspace/dave_ryerson/Domino-AutoDepotOutbound?executionId=6a023485a8280d51ecfe1367

---
+++ autodepotoutbound.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+###########################################################################################
+# 
+# Study   : <all>
+# 
+# Script  : autodepotoutbound.sh
+# 
+# Version : 1.0
+# 
+# Purpose : Call the java program autodepotoutbound to move files from the LSAF depot to various services
+#
+# Date    : 01-Jun-2021
+#
+# Author  : Steve Hege
+#
+# Inputs  : none
+# 
+# Output  : none     
+#
+###########################################################################################
+# Modification History:
+# Date:      Name:           Description:
+# 01Jun2021  Steve Hege      Initial development v1.0
+###########################################################################################
+# Program Notes:
+###########################################################################################
+# AUTOmated Depot Outbound program
+###########################################################################################
+# 
+###########################################################################################
+
+java -jar /domino/datasets/local/Domino-AutoDepotOutbound/autodepotoutbound.jar -c /domino/datasets/local/Domino-AutoDepotOutbound/autodepotoutbound_init.txt >> /mnt/results/logs/autodepotoutbound.log
+
+
+# End of script
